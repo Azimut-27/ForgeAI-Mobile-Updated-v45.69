b80a8a8bcc9fa7ba2ad1c39055a8cf3a9be14935
Azimut-27 <martin.jancar1@gmail.com>
2026-05-17 18:35:59 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
+import WorkoutGenerator from "./WorkoutGenerator_v45.69.jsx";
 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <WorkoutGenerator />
+  </React.StrictMode>
+);
